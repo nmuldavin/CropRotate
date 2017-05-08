51e9c78491cbf96ccaedd898affd1be37bd6893c
Noah Muldavin <nmuldavin@gmail.com>
2017-05-08 16:48:40 -0700

    Load image function and tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-export default function add() { return 44; }
+export const loadImage = src => new Promise((resolve, reject) => {
+  const image = new Image();
+
+  // eslint-disable-next-line
+  Object.assign(image, {
+    src,
+    onload() {
+      resolve(image);
+    },
+    onerror() {
+      reject(new Error('Image not found'));
+    },
+  });
+});
+
+const cropRotate = src => loadImage(src);
+
+export default cropRotate;
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
-import cropRotate from '.';
+import cropRotate, { loadImage } from '.';
 
 describe('Library Index', () => {
   it('should export something', () => {
     expect(cropRotate).to.exist;
   });
+});
+
+describe('(Function) loadImage', () => {
+  const imageUrl = 'someUrl';
+  it('should return a promise that resolves with an Html5 Image object', () =>
+    loadImage(imageUrl).should.eventually.be.an.instanceOf(Image),
+  );
+
+  it('should return an image with the provided sourceUrl', () =>
+    loadImage(imageUrl).should.eventually.have.property('src', imageUrl),
+  );
+
+  it('should reject when the image can\'t be loaded', () =>
+    loadImage().should.be.rejectedWith(Error),
+  );
 });
