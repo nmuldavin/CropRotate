33f1d51f6284e55cd73d9f31e3d4684083051037
Noah Muldavin <nmuldavin@gmail.com>
2017-05-25 22:37:47 -0700

    Clean math

--- src/math/math.js
+++ src/math/math.js
@@ -1,25 +1,22 @@
-const pipe = (val, ...fns) => fns
-  .reduce((accum, fn) => fn(accum), val);
-
 const lengthCheck = (v1, v2) => {
   if (v1.length !== v2.length) throw new Error('Incompatible Dimensions');
   return true;
 };
 
-export const dot = (v1, v2) => lengthCheck(v1, v2) && v1.reduce(
-  (sum, val, index) => sum + (val * v2[index]),
-  0,
-);
-
 const makeTransformMatrix = (angle) => {
-  const absCos = pipe(angle, Math.cos, Math.abs);
-  const absSin = pipe(angle, Math.sin, Math.abs);
+  const absCos = Math.abs(Math.cos(angle));
+  const absSin = Math.abs(Math.sin(angle));
 
   return [
     [absCos, absSin],
     [absSin, absCos],
   ];
 };
+
+export const dot = (v1, v2) => lengthCheck(v1, v2) && v1.reduce(
+  (sum, val, index) => sum + (val * v2[index]),
+  0,
+);
 
 export const matrixVectorMultiply = (mat, vec) => mat.map(row => dot(row, vec));
 
